9446a33632421e63ec973c0ef4f64180639a32c7
Pelayo Arbues <gonzalezpelayo@gmail.com>
2023-08-24 15:04:26 +0200

    Updates colors

--- quartz.config.ts
+++ quartz.config.ts
@@ -30,7 +30,7 @@ const config: QuartzConfig = {
         },
         darkMode: {
           light: "#262626",
-          lightgray: "#A4A5A6",
+          lightgray: "#be616a",
           gray: "#A4A5A6",
           darkgray: "#A4A5A6",
           dark: "#DEDFE0",
