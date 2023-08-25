238ffc9d4465f15b61a1539c88bb709d8d4622e2
Pelayo Arbues <gonzalezpelayo@gmail.com>
2023-08-25 10:46:10 +0200

    Switches to created dates

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     },
     baseUrl: "pelayoarbues.github.io",
     ignorePatterns: ["private", "templates"],
-    defaultDateType: "published",
+    defaultDateType: "created",
     theme: {
       typography: {
         header: "Newsreader",
