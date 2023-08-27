bf55a7337f81260f79b5cb7220300d888957275d
Pelayo Arbues <gonzalezpelayo@gmail.com>
2023-08-27 22:57:41 +0200

    Removes quartz version from footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,7 +14,7 @@ export default ((opts?: Options) => {
       <footer>
         <hr />
         <p>
-          Created with <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a>, © {year}
+          Created with <a href="https://quartz.jzhao.xyz/">, © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
