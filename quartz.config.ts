3e9d6735e15cfda6940d15b0609c240b0aac17bf
Pelayo Arbues <gonzalezpelayo@gmail.com>
2023-09-09 00:40:48 +0200

    Changes baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "pelayoarbues.github.io",
+    baseUrl: "pelayoarbues.com",
     ignorePatterns: ["private", "templates"],
     defaultDateType: "created",
     theme: {
