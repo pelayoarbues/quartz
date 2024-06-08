89088a977b95942a4ea6030a7d47ae4de948456d
Pelayo Arbues <pgarbues@idealista.com>
2024-06-08 20:22:12 +0200

    Adds sections

--- quartz/plugins/emitters/contentIndex.ts
+++ quartz/plugins/emitters/contentIndex.ts
@@ -160,8 +160,9 @@ export const ContentIndex: QuartzEmitterPlugin<Partial<Options>> = (opts) => {
 
       if (opts?.enableRSS) {
         emitted.push(
-          await emit({
-            content: generateRSSFeed(cfg, linkIndex, includedSections, opts.rssLimit), // Pass includedSections
+          await write({
+            ctx,
+            content: generateRSSFeed(cfg,linkIndex,includedSections, opts.rssLimit),
             slug: "index" as FullSlug,
             ext: ".xml",
           }),
@@ -171,8 +172,8 @@ export const ContentIndex: QuartzEmitterPlugin<Partial<Options>> = (opts) => {
       const fp = joinSegments("static", "contentIndex") as FullSlug
       const simplifiedIndex = Object.fromEntries(
         Array.from(linkIndex).map(([slug, content]) => {
-          // remove description and date from content index as nothing downstream
-          // actually uses it. We only keep it in the index as we need it
+          // remove description and from content index as nothing downstream
+          // actually uses it. we only keep it in the index as we need it
           // for the RSS feed
           delete content.description
           delete content.date
